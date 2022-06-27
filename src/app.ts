409d7a721c7d27ec2c5e5df4f8d0abae645a1701
Emre Guneyler <eguneys@gmail.com>
2022-06-27 09:45:51 +0300

    bg

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Quad, Transform, loop } from 'soli2d'
+import { Vec2, Quad, Transform, loop } from 'soli2d'
 import Mavi from './mavi'
 
 import { $observable, $effect } from '@maverick-js/observables'
@@ -21,30 +21,46 @@ const App = (render: any, image: HTMLImage, stage: Transform) => {
   let mavi = new Mavi($update)
 
 
-  let $new = Template.clone
-
-  $new._set_parent(stage)
+  /*
+  let $colors = Template.clone
+  $colors._set_parent(stage)
 
   $effect(() => {
-    mavi.$new.forEach(_ => 
-                      Anim(_))
+    mavi.$news.forEach(_ => 
+                       Anim({
+                        _,
+                        parent: $colors
+                       }))
+  })
+
+ */
+
+
+  let bg = Anim({
+    _: {
+      x: 0,
+      y: 0,
+      qs: [2, 0, 2, 2],
+      size: [320, 180]
+    },
+    parent: stage
   })
 
 
+  function Anim(props) {
 
-
-  function Anim(_: any) {
-
+    let { _ } = props
     let anim = Template.clone
 
     $effect(() => 
             anim.quad = Quad.make(image, ..._.qs))
     $effect(() => anim.x = _.x)
     $effect(() => anim.y = _.y)
+    $effect(() => anim.size = Vec2.make(..._.size))
 
     $effect(() => {
       anim._remove()
-      anim._set_parent(_.parent)
+      anim._set_parent(props.parent)
     })
   }
 }

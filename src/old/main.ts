15869a715448e57646a2de883b6e8f3a00ad92dc
Emre Guneyler <eguneys@gmail.com>
2022-07-22 01:21:39 +0300

    retro

---
+++ src/old/main.ts
@@ -0,0 +1,168 @@
+import sprites_png from '../assets/sprites.png'
+import Canvas from './canvas'
+import P from './play'
+import { Vec2, Rectangle, Transform } from './math'
+import Mouse from './mouse'
+
+import { vSource, fSource } from './shaders'
+import { color_rgb } from './util'
+
+import { set_stage } from './stage'
+
+function load_image(path: string) {
+  return new Promise(resolve => {
+    let res = new Image()
+    res.onload = () => resolve(res)
+    res.src = path
+  })
+}
+
+function loop(fn: (dt: number, dt0: number) => void) {
+  let animation_frame_id
+  let fixed_dt = 1000/60
+  let timestamp0: number | undefined,
+  min_dt = fixed_dt,
+    max_dt = fixed_dt * 2,
+    dt0 = fixed_dt
+
+  let elapsed = 0
+
+  function step(timestamp: number) {
+    let dt = timestamp0 ? timestamp - timestamp0 : fixed_dt
+
+    dt = Math.min(max_dt, Math.max(min_dt, dt))
+
+    fn(dt, dt0)
+
+    dt0 = dt
+    timestamp0 = timestamp
+    animation_frame_id = requestAnimationFrame(step)
+  }
+  animation_frame_id = requestAnimationFrame(step)
+
+  return () => {
+    cancelAnimationFrame(animation_frame_id)
+  }
+}
+
+function make_bounds($element: HTMLElement) {
+
+  let _bounds
+  function set_bounds() {
+    _bounds = $element.getBoundingClientRect()
+  }
+  set_bounds()
+
+  document.addEventListener('scroll', () => set_bounds(), { capture: true, passive: true })
+  window.addEventListener('resize', () => set_bounds(), { passive: true })
+
+
+  return {
+    get bounds() {
+      return _bounds
+    }
+  }
+}
+
+const make_norm_mouse = (has_bounds: any) => {
+  return v => {
+    let { bounds } = has_bounds
+    return Vec2.make(v[0] / bounds.width * 1920, v[1] / bounds.height * 1080)
+  }
+}
+
+export default function app(element: HTMLElement) {
+
+
+  load_image(sprites_png).then(image => {
+
+    let play = new P(new Canvas(element))
+    let mouse = new Mouse(element).init()
+    let stage = new Transform()
+    let bounds = make_bounds(element)
+
+    let ctx = {
+      m_nor: make_norm_mouse(bounds),
+      s: stage,
+      p: play,
+      m: mouse
+    }
+
+    play.glOnce()
+    play.glClear()
+
+    let nb = 256000
+
+    let { program,
+      uniformData,
+      indexBuffer,
+      attributeBuffer,
+      vao } = play.glProgram(vSource, fSource, nb)
+
+    let _indexBuffer = new Uint16Array(nb * 3)
+    let _attributeBuffer = new Float32Array(nb * 6)
+
+    let render = (dt: number, dt0: number) => {
+
+      play.glClear()
+
+      play.glUse(program, uniformData)
+
+
+      let aIndex = 0,
+        iIndex = 0,
+        iNb = 0
+
+      for (let i = 0; i < stage._flat.length; i++) {
+        let el = stage._flat[i]
+        let { world, tint } = el
+
+        let { area, vertexData, indices } = Rectangle.unit.transform(world)
+        let { fsUv } = Rectangle.unit
+
+        if (area < 10) { continue }
+
+        let tintData = color_rgb(tint)
+
+        for (let k = 0; k < vertexData.length; k+= 2) {
+          _attributeBuffer[aIndex++] = vertexData[k]
+          _attributeBuffer[aIndex++] = vertexData[k+1]
+
+          _attributeBuffer[aIndex++] = fsUv[k]
+          _attributeBuffer[aIndex++] = fsUv[k+1]
+
+          _attributeBuffer[aIndex++] = tintData[0]
+          _attributeBuffer[aIndex++] = tintData[1]
+          _attributeBuffer[aIndex++] = tintData[2]
+        }
+
+        for (let k = 0; k < indices.length; k++) {
+          _indexBuffer[iIndex++] = iNb * 4 + indices[k]
+        }
+
+        iNb++;
+      }
+
+      //console.log(_attributeBuffer.slice(0, 100))
+      play.glAttribUpdate(attributeBuffer, _attributeBuffer)
+      play.glIndexUpdate(indexBuffer, _indexBuffer)
+
+      play.glDraw(iNb * 6, vao)
+    }
+
+
+    let ss = set_stage(ctx)
+
+
+    loop((dt: number, dt0: number) => {
+
+      mouse.update(dt, dt0)
+
+      ss.update(dt, dt0)
+
+      stage._update_world()
+      render(dt, dt0)
+    })
+
+  })
+}
--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,31 @@
-export type Color = number
+export const PI = Math.PI;
+export const HALFPI = PI / 2;
+export const THIRDPI = PI / 3;
+export const TAU = PI * 2;
+export const THIRDTAU = TAU / 3;
 
-
-const r_mask = 0xff0000
-const g_mask = 0x00ff00
-const b_mask = 0x0000ff
-
-export function color_rgb(color: Color) {
-
-    let r = ((color & r_mask) >> 16),
-          g = ((color & g_mask) >> 8),
-              b = (color & b_mask)
-
-      return [r, g, b].map(_ => _/255)
+export function rand(min, max) {
+  return Math.random() * (max - min) + min;
 }
+
+export function randInt(min,max) {
+  return Math.floor(rand(min,max));
+}
+
+export function randItem(items) {
+  return items[randInt(0, items.length)];
+}
+
+export function sinh(v) {
+  return (Math.sin(v) + 1) / 2;
+}
+
+export function clamp(v, min, max) {
+  return Math.min(Math.max(v, min), max);
+}
+
+export function round(x) {
+  return Math.round(x * 100) / 100;
+}
+
+export function now() { return Date.now(); }

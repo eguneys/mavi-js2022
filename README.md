2811c8536999800b4c4a00b8f2a7dd5e6a9ad42e
Emre Guneyler <eguneys@gmail.com>
2022-07-23 00:46:07 +0300

    randomness

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## TODO
+
+Color HSB
--- src/play.ts
+++ src/play.ts
@@ -1,5 +1,23 @@
 import { ticks } from './shared'
 import { completed, read, update, tween } from './anim'
+import { Vec2 } from './vec2'
+
+/* https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript */
+const make_random = (seed = 1) => {
+  return () => {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  }
+}
+const random = make_random()
+
+function rnd_int(max: number, rng: RNG = random) {
+  return Math.floor(rng() * max)
+}
+
+function arr_rnd(arr: Array<A>) {
+  return arr[rnd_int(arr.length)]
+}
 
 function arr_remove(arr: Array<A>, a: A) {
   arr.splice(arr.indexOf(a), 1)
@@ -79,7 +97,7 @@ abstract class PlayObjects extends Play {
   }
 }
 
-class WithPlays extends PlayObjects {
+abstract class WithPlays extends PlayObjects {
 
   constructor(readonly plays: AllPlays) {
     super(plays.ctx)
@@ -101,10 +119,24 @@ class WithPlays extends PlayObjects {
     if (group) {
       arr_remove(group, this)
     }
+    this._dispose()
   }
+
+
+  abstract _dispose(): void;
 }
 
 class VanishCircle extends WithPlays {
+
+  static make = (base, group, v_pos: Vec2, x, y, radius, color) => {
+    return new VanishCircle(base)._set_data({ 
+      group,
+      v_pos,
+      x,
+      y,
+      radius: radius + 100, 
+      color }).init()
+  }
 
   _init() {
     let { radius } = this.data
@@ -122,23 +154,36 @@ class VanishCircle extends WithPlays {
   }
 
   _draw() {
-    let { x, y, color } = this.data
+    let { v_pos, x, y, color } = this.data
     let [radius] = read(this._rt)
-    this.g.fc(x, y, radius, color)
+    this.g.fc(v_pos.x + x, v_pos.y + y, radius, color)
+  }
+
+
+  _dispose() {
   }
 }
+
+let colors = ['red', 'white']
+let radiuss = [0, 10, 20]
+let xs = [0, 10, 20]
+let ys = [0, 10, 20]
+let makes = [VanishCircle, [xs, ys, radiuss, colors]]
+
+//red xy .6 white xy .3 black xy
+//white xy .5 red xy .4 black xy
 
 export default class AllPlays extends PlayObjects {
 
   _init() {
-    new VanishCircle(this)._set_data({ 
-      group: this.objects, 
-      x: 100,
-      y: 100,
-      radius: 100, 
-      color: 'red' }).init()
-  }
+      }
 
-  _update(dt: number, dt0: number) {}
+  _update(dt: number, dt0: number) {
+    let [_make, rest] = makes
+
+    let _rest = rest.map(_ => arr_rnd(_))
+    let pos = Vec2.make(300, 300)
+    _make.make(this, this.objects, pos, ..._rest)
+  }
   _draw() {}
 }
